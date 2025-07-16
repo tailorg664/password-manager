1cb8f00af367f79480612dff16ff1777da360958
Gaurav Tailor <tailorg664@gmail.com>
2025-07-17 00:05:06 +0530

    minor change 6

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(
   cors({
-    origin: "https://password-manager-mzfk.vercel.app/",
+    origin: "https://password-manager-mzfk.vercel.app",
     credentials: true,
   })
 );
