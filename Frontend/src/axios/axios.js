273be6dd36e4fd4561bdbd4df3f1e61cf04bdabb
Gaurav Tailor <tailorg664@gmail.com>
2025-07-16 23:42:59 +0530

    minor change 5

--- Frontend/src/axios/axios.js
+++ Frontend/src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
-     baseURL: apiUrl,
+     baseURL: `${apiUrl}/api`,
      withCredentials:true,
      headers:{
           "Content-Type": 'application/json'
