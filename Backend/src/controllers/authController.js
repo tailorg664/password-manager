7fda626e5eac132de51f074955b59c875e1b1ee2
Gaurav Tailor <tailorg664@gmail.com>
2025-07-18 00:34:36 +0530

    local storage function added

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -90,7 +90,7 @@ const login = async (req, res) => {
 
 const logout = async (req, res) => {
   try {
-    res.clearCookie("token", { httpOnly: true, secure: false, sameSite: "None" });
+    res.clearCookie("token", { httpOnly: true, secure: true, sameSite: "None" });
     res.status(200).json({
       success: true,
       message: "User logged out successfully",
--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -1,79 +1,79 @@
-import {create} from "zustand";
+import { create } from "zustand";
 import axiosInstance from "../axios/axios.js";
 
-const useAuthStore = create((set,get)=>({
-     authUser:null,//imp
-     isCheckingAuth:false,
-     isLoggingIn:false,isLoggingOut: false,
-     isSigningUp:false,
-     error:null,
-      checkAuth: async () => {
-          set({ isCheckingAuth: true, error: null });
+const useAuthStore = create((set, get) => ({
+  authUser: null, //imp
+  isCheckingAuth: false,
+  isLoggingIn: false,
+  isLoggingOut: false,
+  isSigningUp: false,
+  error: null,
+  checkAuth: async () => {
+    set({ isCheckingAuth: true, error: null });
 
-            try {
-              const res = await axiosInstance.get('/auth/check');
+    try {
+      const res = await axiosInstance.get("/auth/check");
 
-              // Optional: log once to confirm structure
-              console.log("✅ checkAuth user:", res.data.user);
-              set({ authUser: res.data.user,error:null });
+      // Optional: log once to confirm structure
+      console.log("✅ checkAuth user:", res.data.user);
+      set({ authUser: res.data.user, error: null });
+    } catch (error) {
+      const errMsg =
+        error?.response?.data?.message || "Unauthorized or session expired";
 
-            } catch (error) {
-                   const errMsg =
-                     error?.response?.data?.message || "Unauthorized or session expired";
+      console.error("❌ checkAuth error:", errMsg);
 
-                   console.error("❌ checkAuth error:", errMsg);
+      set({ authUser: null, error: errMsg });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
 
-                   set({ authUser: null, error: errMsg });
-                 }
-            finally {
-                   set({ isCheckingAuth: false });
-          }
-},
-
-       login: async (formData) => {
+  login: async (formData) => {
     set({ isLoggingIn: true, error: null });
     try {
-         const res = await axiosInstance.post('/auth/login', formData);
-         set({ authUser: res.data.user });
-       } catch (err) {
-         set({ error: err.response?.data?.message || 'Login failed' });
-       } finally {
-         set({ isLoggingIn: false });
-       }
-     },
+      const res = await axiosInstance.post("/auth/login", formData);
+      localStorage.setItem("token", res.data.token);
+      set({ authUser: res.data.user });
+    } catch (err) {
+      set({ error: err.response?.data?.message || "Login failed" });
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
 
-     logout:async()=>{
-          set({ isLoggingOut: true, error: null });
+  logout: async () => {
+    set({ isLoggingOut: true, error: null });
 
-          try{
-               await axiosInstance.post('/auth/logout');
-               set({authUser:null,error:null})
-               console.log("✅ Logged out successfully");
-          }catch (err){
-               const errMsg = err?.response?.data?.message || "Logout failed";
-               set({ error: errMsg });
-               console.error("❌ Logout error:", errMsg);
-          }finally {
-               set({ isLoggingOut: false });
-             }
-     },
-     signup: async (credentials)=>{
-          set({isSigningUp:true})
-          try{
-               const response = await axiosInstance.post("/auth/signup",credentials)
-               const user = response.data.user
-               set({authUser: user})
-               console.log(get().authUser)
-          }
-          catch (error){
-               set({error: error?.response?.data?.message || "Signup failed"})
-          }
-          finally {
-               set({isSigningUp:false})
-          }
-     }
-}))
-export default  useAuthStore;
+    try {
+      await axiosInstance.post("/auth/logout");
+      localStorage.removeItem("token");
+      set({ authUser: null, error: null });
+      console.log("✅ Logged out successfully");
+    } catch (err) {
+      const errMsg = err?.response?.data?.message || "Logout failed";
+      set({ error: errMsg });
+      console.error("❌ Logout error:", errMsg);
+    } finally {
+      set({ isLoggingOut: false });
+    }
+  },
+  signup: async (credentials) => {
+    set({ isSigningUp: true });
+    try {
+      const response = await axiosInstance.post("/auth/signup", credentials);
+      const user = response.data.user;
+      localStorage.setItem("token", response.data.token);
+      set({ authUser: user });
+      console.log(get().authUser);
+    } catch (error) {
+      set({ error: error?.response?.data?.message || "Signup failed" });
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+}));
+export default useAuthStore;
 
 //user data: same format:
 // { data:{_id: '12345', name: 'John Doe', email:'tailorg '}}
