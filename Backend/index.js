03812ec8702fbb0838fca3cd90b7509a5db72c91
Gaurav Tailor <tailorg664@gmail.com>
2025-08-26 17:29:10 +0530

    Cors policy changes

--- Backend/index.js
+++ Backend/index.js
@@ -3,23 +3,31 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./src/db/database.js";
 import http from "http";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 dotenv.config();
 connectDB()
   .then(() => {
     console.log("MongoDB connected successfully");
   })
   .catch((err) => {
-    console.log("the error is: ",err.message);
+    console.log("the error is: ", err.message);
   });
 const app = express();
 const server = http.createServer(app);
-const origin = process.env.CORS_ORIGIN
+const origin = process.env.CORS_ORIGIN;
+const extensionOrigin = process.env.EXTENSION_ID;
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+const allowedOrigins = [origin, extensionOrigin];
 app.use(
   cors({
-    origin: `${origin}`,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
@@ -27,7 +35,7 @@ app.use(
 import authRoutes from "./src/routes/authRoute.js";
 import passwordRoute from "./src/routes/passwordRoute.js";
 app.use("/api/auth", authRoutes);
-app.use("/api/home",passwordRoute);
+app.use("/api/home", passwordRoute);
 //listening to server
 const PORT = process.env.PORT;
 server.listen(PORT, () => {
